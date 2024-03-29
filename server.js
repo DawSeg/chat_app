36ce3ba4cdb0ca25e2a897a82df0baf9a2c730c9
DawSeg <buler43221@gmail.com>
2024-03-29 18:22:03 +0100

    update_server

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+
+let messages = [];
+
+app.use(express.static('client'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
 
 app.listen(8000, () => {
   console.log('server is running on port 8000');
